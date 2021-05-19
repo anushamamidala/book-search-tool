4eb38a507ae3a1a9ab90bfad05aa150a7c9fbe5f
anushamamidala <anushamamidala817@gmail.com>
2021-05-19 11:28:26 -0400

    Added default image

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,25 +1,38 @@
 import './style.css'
-import defaultImg from '../../assets/default.jpg';
+import defaultImg from '../../assets/default.jpg'
 import CONSTANTS from '../../utils/constant'
 
-const Book = ({booksList}) => {
+const Book = ({ booksList }) => {
+  console.log(booksList)
   return (
-    <div className="books-container">
+    <div className='books-container'>
       {booksList &&
         booksList.length > 0 &&
         booksList.map(book => {
-           
           return (
-            book?.cover_i ?
             <div className='books-card' key={book.key}>
-              <img src={CONSTANTS.BOOKS_IMAGE_API + book?.cover_i +'-L.jpg'} height="60%" width="60%" alt="Book Image" />
-              <div className="book-details">
-                <span className="book-title">{book.title}</span>
-                {
-                  book?.author_name && book?.author_name.length > 0 ? <span>{book?.author_name[0]}</span> : null
-                }                
+              {book?.cover_i ? (
+                <img
+                  src={CONSTANTS.BOOKS_IMAGE_API + book?.cover_i + '-L.jpg'}
+                  height='60%'
+                  width='60%'
+                  alt='Book Image'
+                />
+              ) : (
+                <img
+                  src={defaultImg}
+                  height='60%'
+                  width='60%'
+                  alt='Book Image'
+                />
+              )}
+              <div className='book-details'>
+                <span className='book-title'>{book.title}</span>
+                {book?.author_name && book?.author_name.length > 0 ? (
+                  <span>{book?.author_name[0]}</span>
+                ) : null}
               </div>
-            </div>  : null
+            </div>
           )
         })}
     </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,9 +18,30 @@ const Home = () => {
         searchKey +
         '&fields=key,cover_i,title,author_name,name,publish_date,isbn&mode=everything'
     )
-    console.log(result)
     setBooksList(result?.data?.docs)
     setIsLoading(false)
+  }
+
+  const onChangeInSelect = val => {
+    if (val === 'sortAscending') {
+      const books = booksList.sort((a, b) =>
+        a.title.toLowerCase() > b.title.toLowerCase()
+          ? 1
+          : b.title.toLowerCase() > a.title.toLowerCase()
+          ? -1
+          : 0
+      )
+      setBooksList([...books])
+    } else if (val === 'sortDescending') {
+      const books = booksList.sort((a, b) =>
+        a.title.toLowerCase() > b.title.toLowerCase()
+          ? -1
+          : b.title.toLowerCase() > a.title.toLowerCase()
+          ? 1
+          : 0
+      )
+      setBooksList([...books])
+    }
   }
   return !isLoading ? (
     <div className='main-container'>
@@ -35,6 +56,23 @@ const Home = () => {
         <button className='search-button' onClick={handleSearch}>
           Search
         </button>
+        <select
+          className='sort-select-drop'
+          onChange={e => onChangeInSelect(e.target.value)}
+        >
+          <option disabled selected>
+            Select option
+          </option>
+          <option value='sortAscending'>
+            Sort Alphabetically A-Z by Title
+          </option>
+          <option value='sortDescending'>
+            Sort Alphabetically Z-A by Title
+          </option>
+          <option value='sortByPublishedDate'>
+            Sort by Recent Published Date
+          </option>
+        </select>
       </div>
       <Book booksList={booksList} />
     </div>
